5cbb1596fc777642e290c3e9c8e6dcf6a86d3c85
AlexandraKartashovaFusion <alexandra.kartasheva@fusion-tech.pro>
2021-03-10 10:33:28 +0300

    services

---
+++ src/Boilerplate.Services/IItemService.cs
@@ -0,0 +1,16 @@
+﻿namespace Boilerplate.Services
+{
+    using System.Threading.Tasks;
+    using Boilerplate.Models;
+
+    public interface IItemService
+    {
+        Task Add(ItemAddDto itemDto);
+
+        Task Update(ItemUpdateDto itemDto);
+
+        Task Put(ItemUpdateDto itemDto);
+        
+        Task Delete(int id);
+    }
+}
---
+++ src/Boilerplate.Services/ItemService.cs
@@ -0,0 +1,91 @@
+﻿namespace Boilerplate.Services
+{
+    #region << Using >>
+
+    using System;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Boilerplate.Domain;
+    using Boilerplate.Models;
+
+    #endregion
+
+    public class ItemService : IItemService
+    {
+        #region Properties
+
+        //private readonly IRepository<Item> _repo;
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        #endregion
+
+        #region Constructors
+
+        public ItemService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        #endregion
+
+        #region Interface Implementations
+
+        public Task Add(ItemAddDto itemDto)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<ItemAddDto, Item>()
+               .ForMember("Value", opt => opt.MapFrom(c => c.Value))
+               .ForMember("Status", opt => opt.MapFrom(c => c.Status))
+               .ForMember("CreateDate", opt => opt.MapFrom(c => c.CreateDate)));
+
+            var mapper = new Mapper(config);
+            var item = mapper.Map<ItemAddDto, Item>(itemDto);
+
+            this._unitOfWork.Repository<Item>().Add(item);
+            return this._unitOfWork.Repository<Item>().SaveChangesAsync();
+        }
+
+        public Task Update(ItemUpdateDto itemDto)
+        {
+            var item = this._unitOfWork.Repository<Item>().Get(itemDto.Id);
+
+            if (item == null)
+                throw new ArgumentNullException();
+
+            item.Status = itemDto.Status;
+            item.UpdateDate = DateTime.UtcNow;
+            item.Value = itemDto.Value;
+
+            this._unitOfWork.Repository<Item>().Update(item);
+            return this._unitOfWork.Repository<Item>().SaveChangesAsync();
+        }
+
+        public  Task Put(ItemUpdateDto itemDto)
+        {
+            var item =  this._unitOfWork.Repository<Item>().Get(itemDto.Id);
+
+            if (item == null)
+                throw new ArgumentNullException();
+
+            item.Status = itemDto.Status;
+            item.UpdateDate = DateTime.UtcNow;
+            item.Value = itemDto.Value;
+
+            this._unitOfWork.Repository<Item>().Update(item);
+            return this._unitOfWork.Repository<Item>().SaveChangesAsync();
+        }
+
+        public Task Delete(int id)
+        {
+            var item = this._unitOfWork.Repository<Item>().Get(id);
+
+            if (item == null)
+                throw new ArgumentNullException();
+
+            this._unitOfWork.Repository<Item>().Delete(item);
+            return this._unitOfWork.Repository<Item>().SaveChangesAsync();
+        }
+
+        #endregion
+    }
+}
